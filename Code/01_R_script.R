2ad2ed5fad066764997f9b52df8b688e4bfd80b8
paolaztorres <paolazanella.br@gmail.com>
2026-09-24 12:32:04 +0200

    Create 01_R_script.R

---
+++ Code/01_R_script.R
@@ -0,0 +1,13 @@
+# first operation 
+2 + 3
+
+# first object
+cecilia <- 2 + 3
+
+# second object
+julius <- 4 + 6
+
+# additional operations 
+cecilia + julius # 2 + 3 + 4 + 6
+cecilia * julius # (2 + 3) * (4 + 6)
+cecilia ^ julius # (2 + 3) ^ (4 + 6)
